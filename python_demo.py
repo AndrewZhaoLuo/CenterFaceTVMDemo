538d69b74d3061380ff51a4d14b3841d828aaaa5
Andrew Luo <andrew.zhao.luo@gmail.com>
2021-12-01 17:24:37 -0800

    multithreaded python demo

--- python_demo.py
+++ python_demo.py
@@ -1,4 +1,6 @@
+import queue
 import time
+from threading import Thread
 
 import cv2
 
@@ -8,82 +10,146 @@ TARGET_WIDTH = 640
 TARGET_HEIGHT = 640
 
 
-def camera(runner, threshold=0.5):
-    cap = cv2.VideoCapture(0)
-    _, frame = cap.read()
+class CameraDemo:
+    """Multi-threaded python centerface detection demo."""
 
-    # assume w > h
-    h, w = frame.shape[:2]
-    scale = TARGET_WIDTH / h
-    new_width = int(scale * w)
-    new_height = int(scale * h)
+    def __init__(self, runner: centerface_utils.CenterFaceNoDetection) -> None:
+        self.keep_going = True
+        self.runner = runner
 
-    # For centercrop
-    left = (new_width - TARGET_WIDTH) // 2
-    top = (new_height - TARGET_HEIGHT) // 2
-    right = (new_width + TARGET_WIDTH) // 2
-    bottom = (new_height + TARGET_HEIGHT) // 2
+    def capture_frame(self, cap, queue):
+        """Thread calsl function which captures data from webcam and places into queue"""
+        while self.keep_going:
+            _, img = cap.read()
+            queue.put(img)
 
-    # For displaying FPS
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    prev_frame_time = 0
-    cur_frame_time = 0
+    def process_frame(
+        self, runner, processing_func, input_queue, output_queue, threshold
+    ):
+        """Thread which detections and overlays results, add it to queue for rendering"""
+        while self.keep_going:
+            if input_queue.empty():
+                continue
+            frame = input_queue.get()
+            frame = processing_func(frame)
 
-    avg_fps = [0] * 10
-    cur_frame = 0
+            np_array = cv2.dnn.blobFromImage(
+                frame,
+                scalefactor=1.0,
+                size=(TARGET_WIDTH, TARGET_HEIGHT),
+                mean=(0, 0, 0),
+                swapRB=True,
+                crop=True,
+            )
+            start = time.time()
+            detections, landmarks = runner(
+                np_array, TARGET_HEIGHT, TARGET_WIDTH, threshold=threshold
+            )
+            end = time.time()
+            print(f"Processing frame too {(end - start) * 1000} ms")
 
-    while True:
+            # Draw predictions and show frame
+            for det in detections:
+                boxes, _ = det[:4], det[4]
+                cv2.rectangle(
+                    frame,
+                    (int(boxes[0]), int(boxes[1])),
+                    (int(boxes[2]), int(boxes[3])),
+                    (2, 255, 0),
+                    3,
+                )
+            for lm in landmarks:
+                for i in range(0, 5):
+                    cv2.circle(
+                        frame, (int(lm[i * 2]), int(lm[i * 2 + 1])), 4, (0, 0, 255), -1
+                    )
+
+            output_queue.put(frame)
+
+    def run(self, threshold=0.5):
+        cap = cv2.VideoCapture(0)
+
+        cap_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+        cap_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        cap.set(cv2.CAP_PROP_FPS, 60)
+        cap_fps = cap.get(cv2.CAP_PROP_FPS)
+        print("* Capture width:", cap_width)
+        print("* Capture height:", cap_height)
+        print("* Capture FPS:", cap_fps)
+
         _, frame = cap.read()
 
-        # Resize and center crop frame
-        frame = cv2.resize(frame, (new_width, new_height))
-        frame = frame[top:bottom, left:right]
+        # assume w > h
+        h, w = frame.shape[:2]
+        scale = TARGET_WIDTH / h
+        new_width = int(scale * w)
+        new_height = int(scale * h)
 
-        # Run model
-        np_array = cv2.dnn.blobFromImage(
-            frame,
-            scalefactor=1.0,
-            size=(TARGET_WIDTH, TARGET_HEIGHT),
-            mean=(0, 0, 0),
-            swapRB=True,
-            crop=True,
+        # For centercrop
+        left = (new_width - TARGET_WIDTH) // 2
+        top = (new_height - TARGET_HEIGHT) // 2
+        right = (new_width + TARGET_WIDTH) // 2
+        bottom = (new_height + TARGET_HEIGHT) // 2
+
+        # initial queue for webcam data
+        frames_queue = queue.Queue(maxsize=100)
+
+        # queue after we've streamed it to real-time feed
+        ready_for_processing_queue = queue.Queue(maxsize=100)
+
+        # queue for processed frames with prediction overlays
+        processed_frames_queue = queue.Queue(maxsize=100)
+
+        # start thread to capture data from webcam
+        capture_thread = Thread(
+            target=self.capture_frame,
+            args=(
+                cap,
+                frames_queue,
+            ),
+            daemon=True,
         )
-        detections, landmarks = runner(
-            np_array, TARGET_HEIGHT, TARGET_WIDTH, threshold=threshold
+        capture_thread.start()
+
+        def processing_func(cv2_frame):
+            # Resize and center crop frame
+            frame = cv2.resize(cv2_frame, (new_width, new_height))
+            frame = frame[top:bottom, left:right]
+            return frame
+
+        # start thread to process images with model
+        processing_thread = Thread(
+            target=self.process_frame,
+            args=(
+                self.runner,
+                processing_func,
+                ready_for_processing_queue,
+                processed_frames_queue,
+                threshold,
+            ),
+            daemon=True,
         )
+        processing_thread.start()
 
-        # Draw predictions and show frame
-        for det in detections:
-            boxes, _ = det[:4], det[4]
-            cv2.rectangle(
-                frame,
-                (int(boxes[0]), int(boxes[1])),
-                (int(boxes[2]), int(boxes[3])),
-                (2, 255, 0),
-                3,
-            )
-        for lm in landmarks:
-            for i in range(0, 5):
-                cv2.circle(
-                    frame, (int(lm[i * 2]), int(lm[i * 2 + 1])), 4, (0, 0, 255), -1
-                )
+        while self.keep_going:
+            if not frames_queue.empty():
+                img_real_time = frames_queue.get()
+                if img_real_time is not None:
+                    cv2.imshow("realtime", img_real_time)
+                    ready_for_processing_queue.put(img_real_time)
 
-        # calculate fps
-        cur_frame_time = time.time()
-        point_fps = 1 / (cur_frame_time - prev_frame_time)
-        prev_frame_time = cur_frame_time
-        avg_fps[cur_frame % len(avg_fps)] = point_fps
-        fps = int(sum(avg_fps) / min(len(avg_fps), cur_frame + 1))
+            if not processed_frames_queue.empty():
+                img_processed = processed_frames_queue.get()
+                if img_processed is not None:
+                    cv2.imshow("predicted", img_processed)
 
-        cv2.putText(frame, str(fps), (7, 70), font, 3, (100, 255, 0), 3, cv2.LINE_AA)
-        cv2.imshow("out", frame)
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                keep_going = False
+                break
 
-        cur_frame += 1
-        # Press Q on keyboard to stop recording
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
-
-    cap.release()
+        cap.release()
+        capture_thread.join()
+        processing_thread.join()
 
 
 if __name__ == "__main__":
@@ -96,4 +162,6 @@ if __name__ == "__main__":
     )
     dummy_runner = centerface_utils.CenterFaceNoDetection()
 
-    camera(onnx_runner)
+    # Change runners at will
+    demo = CameraDemo(tvm_runner_fp16)
+    demo.run()
